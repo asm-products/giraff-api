efcede5e61ae3c7caf2240734f1eab1a1a70577a
Dave Newman <dave@snappyco.de>
2015-02-06 09:43:57 -0800

    Add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,42 @@
 Image.create([{
     name: 'Googly Eyes',
     original_source: 'http://i.imgur.com/smFgbd1.gif'
-  }])
+  }, {
+    name: "Don't worry we'll stick together",
+    original_source: 'http://i.imgur.com/1s72yIt.gif'
+  }, {
+    name: "A security camera persons dream",
+    original_source: 'http://i.imgur.com/J8MZcVv.gif'
+  }, {
+    name: "Underwater nightmare incarnate",
+    original_source: 'http://i.imgur.com/0fOAkVy.gif'
+  }, {
+    name: "This is exactly why we have this technology",
+    original_source: 'http://i.imgur.com/kdEUjWe.gif'
+  }, {
+    name: "Satisfaction.",
+    original_source: 'http://i.imgur.com/H6J7wpe.gif'
+  }, {
+    name: "Oh hey there Reddit, How relaxed are you right now?",
+    original_source: 'http://i.imgur.com/KQb94Bi.gif'
+  }, {
+    name: "Voyager 1's approach to Jupiter over a period of 60 days",
+    original_source: 'http://i.imgur.com/2ViwH0J.gif'
+  }, {
+    name: "Escalating cuteness",
+    original_source: 'http://i.imgur.com/jlcAfFT.gif'
+  }, {
+    name: "We're gonna need a bigger stage",
+    original_source: 'http://i.imgur.com/Rjpzo0g.gif'
+  }, {
+    name: "Mlem, mlem, mlem",
+    original_source: 'http://i.imgur.com/yH2OJZy.gif'
+  }, {
+    name: "Truly Epic Snowmobile Drop",
+    original_source: 'http://i.imgur.com/7Mm3Lmn.gif'
+  }, {
+    name: "Fun & serious cat.",
+    original_source: 'http://i.imgur.com/5IB0RDt.gif'
+  }
+
+])
