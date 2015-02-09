5aaf1b81aeef5b46a74204505165ef69b76ca4e2
Dave Newman <dave@snappyco.de>
2015-02-08 18:02:35 -0800

    Skip auth for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
-  acts_as_token_authentication_handler_for User
+  # acts_as_token_authentication_handler_for User
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations", :sessions => "sessions"}
+  devise_for :users, controllers: {registrations: "registrations", sessions: "sessions"}
   resources :images, only: [:index]
 end
