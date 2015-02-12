f13b35c41280cce39e592220df90082a5061a66d
Ben <hello@benshyong.com>
2015-02-12 12:03:49 -0800

    fix db seeds, add shortcode endpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,68 @@
 Image.create([{
     name: 'Googly Eyes',
-    original_source: 'http://i.imgur.com/smFgbd1.gif'
+    original_source: 'http://i.imgur.com/smFgbd1.gif',
+    bytes: 0,
+    shortcode: "smFgbd1"
   }, {
     name: "Don't worry we'll stick together",
-    original_source: 'http://i.imgur.com/1s72yIt.gif'
+    original_source: 'http://i.imgur.com/1s72yIt.gif',
+    bytes: 0,
+    shortcode: "1s72yIt"
   }, {
     name: "A security camera persons dream",
-    original_source: 'http://i.imgur.com/J8MZcVv.gif'
+    original_source: 'http://i.imgur.com/J8MZcVv.gif',
+    bytes: 0,
+    shortcode: "J8MZcVv"
   }, {
     name: "Underwater nightmare incarnate",
-    original_source: 'http://i.imgur.com/0fOAkVy.gif'
+    original_source: 'http://i.imgur.com/0fOAkVy.gif',
+    bytes: 0,
+    shortcode: "0fOAkVy"
   }, {
     name: "This is exactly why we have this technology",
-    original_source: 'http://i.imgur.com/kdEUjWe.gif'
+    original_source: 'http://i.imgur.com/kdEUjWe.gif',
+    bytes: 0,
+    shortcode: "kdEUjWe"
   }, {
     name: "Satisfaction.",
-    original_source: 'http://i.imgur.com/H6J7wpe.gif'
+    original_source: 'http://i.imgur.com/H6J7wpe.gif',
+    bytes: 0,
+    shortcode: "H6J7wpe"
   }, {
     name: "Oh hey there Reddit, How relaxed are you right now?",
-    original_source: 'http://i.imgur.com/KQb94Bi.gif'
+    original_source: 'http://i.imgur.com/KQb94Bi.gif',
+    bytes: 0,
+    shortcode: "KQb94Bi"
   }, {
     name: "Voyager 1's approach to Jupiter over a period of 60 days",
-    original_source: 'http://i.imgur.com/2ViwH0J.gif'
+    original_source: 'http://i.imgur.com/2ViwH0J.gif',
+      bytes: 0,
+    shortcode: "2ViwH0J"
   }, {
     name: "Escalating cuteness",
-    original_source: 'http://i.imgur.com/jlcAfFT.gif'
+    original_source: 'http://i.imgur.com/jlcAfFT.gif',
+    bytes: 0,
+    shortcode: "jlcAfFT"
   }, {
     name: "We're gonna need a bigger stage",
-    original_source: 'http://i.imgur.com/Rjpzo0g.gif'
+    original_source: 'http://i.imgur.com/Rjpzo0g.gif',
+    bytes: 0,
+    shortcode: "Rjpzo0g"
   }, {
     name: "Mlem, mlem, mlem",
-    original_source: 'http://i.imgur.com/yH2OJZy.gif'
+    original_source: 'http://i.imgur.com/yH2OJZy.gif',
+    bytes: 0,
+    shortcode: "yH2OJZy"
   }, {
     name: "Truly Epic Snowmobile Drop",
-    original_source: 'http://i.imgur.com/7Mm3Lmn.gif'
+    original_source: 'http://i.imgur.com/7Mm3Lmn.gif',
+    bytes: 0,
+    shortcode: "7Mm3Lmn"
   }, {
     name: "Fun & serious cat.",
-    original_source: 'http://i.imgur.com/5IB0RDt.gif'
+    original_source: 'http://i.imgur.com/5IB0RDt.gif',
+    bytes: 0,
+    shortcode: "5IB0RDt"
   }
 
 ])
