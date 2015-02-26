d501eb23a2802d42a964e6edb02c4ab33c8e7e4f
Ben <hello@benshyong.com>
2015-02-26 14:25:52 -0800

    remove small scope

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,14 +4,14 @@ class ImagesController < ApplicationController
   skip_before_filter :authenticate_user_from_token!, only: [:shortcode]
 
   def index
-    @all =  Image.small.unseen_by(current_user).super_hot.limit(180)
-    @all << Image.small.unseen_by(current_user).least_seen.limit(90)
-    @all << Image.small.unseen_by(current_user).rising.limit(90)
+    @all =  Image.unseen_by(current_user).super_hot.limit(180)
+    @all << Image.unseen_by(current_user).least_seen.limit(90)
+    @all << Image.unseen_by(current_user).rising.limit(90)
     respond_with  Kaminari.paginate_array(@all.uniq.shuffle).page(params[:page])
   end
 
   def favorites
-    respond_with Image.small.faved_by(current_user).page(params[:page])
+    respond_with Image.faved_by(current_user).page(params[:page])
   end
 
   def shortcode
