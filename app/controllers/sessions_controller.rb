fae8a91e108ac9b4bc6ae08f1710f3516ecd9e31
Ben <hello@benshyong.com>
2015-03-02 16:14:38 -0800

    fix typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     return invalid_login_attempt unless @user.valid?
 
     if @user.valid_password?(params[:password])
-      @user.update! authentication_token: user.generate_auth_token
+      @user.update! authentication_token: @user.generate_auth_token
       sign_in @user, store: false
       
       render json: { authentication_token: @user.authentication_token }, status: :ok
