ccacb2ba4bf4aa3952dacc465b26d64f33ceada6
Ben <hello@benshyong.com>
2015-04-02 14:50:08 -0700

    paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     },
-    url: ":s3_host_alias",
+    url: ":s3_alias_url",
     s3_host_alias: ENV['CLOUDFRONT_URL']
   }
 end
